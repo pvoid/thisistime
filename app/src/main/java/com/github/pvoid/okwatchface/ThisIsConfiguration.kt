3cb05b5a06cfc90376b4fb4a7bfbdd7e802840ef
Dmitrii Petukhov <91985+pvoid@users.noreply.github.com>
2023-01-03 22:27:44 +0100

    Add simple ambient mode and a setting to disable it

--- app/src/main/java/com/github/pvoid/okwatchface/ThisIsConfiguration.kt
+++ app/src/main/java/com/github/pvoid/okwatchface/ThisIsConfiguration.kt
@@ -1,10 +1,17 @@
 package com.github.pvoid.okwatchface
 
 import android.graphics.RectF
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.wear.watchface.complications.data.ComplicationType
 
+const val STYLE_KEY_AMBIENT = "ambient_type_setting"
 const val STYLE_KEY_LAYOUT = "complication_layout_setting"
+
+enum class AmbientModeType {
+    Minimalistic,
+    Background
+}
 
 enum class ComplicationSlotInfo(
     val id: Int,
@@ -56,31 +63,37 @@ enum class ComplicationSlotInfo(
 enum class ComplicationLayout(
     val id: String,
     @StringRes val title: Int,
+    @DrawableRes val icon: Int,
     val complications: List<ComplicationSlotInfo>
 ) {
     COMPAT_RIGHT(
         id = "compat_right",
         title = R.string.layout_compat_right,
+        icon = R.drawable.layout_compat_right,
         complications = listOf(ComplicationSlotInfo.RIGHT_CENTER)
     ),
     COMPAT_LEFT(
         id = "compat_left",
         title = R.string.layout_compat_left,
+        icon = R.drawable.layout_compat_left,
         complications = listOf(ComplicationSlotInfo.LEFT_TOP)
     ),
     FULL_RIGHT(
         id = "full_right",
         title = R.string.layout_full_right,
+        icon = R.drawable.layout_full_right,
         complications = listOf(ComplicationSlotInfo.RIGHT_TOP, ComplicationSlotInfo.RIGHT_BOTTOM)
     ),
     FULL(
         id = "full",
         title = R.string.layout_full,
+        icon = R.drawable.layout_full,
         complications = listOf(ComplicationSlotInfo.RIGHT_TOP, ComplicationSlotInfo.RIGHT_BOTTOM, ComplicationSlotInfo.LEFT_TOP)
     ),
     CLEAR(
         id = "clear",
         title = R.string.layout_clear,
+        icon = R.drawable.layout_clear,
         complications = listOf()
     );
 }
--- app/src/main/java/com/github/pvoid/okwatchface/ThisIsWatchFace.kt
+++ app/src/main/java/com/github/pvoid/okwatchface/ThisIsWatchFace.kt
@@ -2,6 +2,7 @@ package com.github.pvoid.okwatchface
 
 import android.content.Context
 import android.graphics.*
+import android.graphics.drawable.Icon
 import android.view.SurfaceHolder
 import androidx.wear.watchface.*
 import androidx.wear.watchface.complications.ComplicationSlotBounds
@@ -16,6 +17,7 @@ import androidx.wear.watchface.style.UserStyleSetting.Option
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 import java.time.ZonedDateTime
 
@@ -49,26 +51,34 @@ class ThisIsWatchFace : WatchFaceService() {
             UserStyleSetting.ComplicationSlotsUserStyleSetting.ComplicationSlotsOption(
                 Option.Id(info.id),
                 resources.getString(info.title),
-                icon = null,
+                icon = Icon.createWithResource(applicationContext, info.icon),
                 overlays
             )
         }
 
-        return UserStyleSchema(
-            listOf(
-                UserStyleSetting.ComplicationSlotsUserStyleSetting(
-                    UserStyleSetting.Id(STYLE_KEY_LAYOUT),
-                    resources.getString(R.string.option_layout_name),
-                    resources.getString(R.string.option_layout_desc),
-                    icon = null,
-                    complicationConfig = options,
-                    listOf(
-                        WatchFaceLayer.COMPLICATIONS,
-                        WatchFaceLayer.COMPLICATIONS_OVERLAY
-                    ),
+        val settings = mutableListOf<UserStyleSetting>(
+            UserStyleSetting.BooleanUserStyleSetting(
+                UserStyleSetting.Id(STYLE_KEY_AMBIENT),
+                resources.getString(R.string.option_ambient_type_name),
+                resources.getString(R.string.option_ambient_type_desc),
+                icon = null,
+                listOf(),
+                true
+            ),
+            UserStyleSetting.ComplicationSlotsUserStyleSetting(
+                UserStyleSetting.Id(STYLE_KEY_LAYOUT),
+                resources.getString(R.string.option_layout_name),
+                resources.getString(R.string.option_layout_desc),
+                icon = null,
+                complicationConfig = options,
+                listOf(
+                    WatchFaceLayer.COMPLICATIONS,
+                    WatchFaceLayer.COMPLICATIONS_OVERLAY
                 ),
-            )
+            ),
         )
+
+        return UserStyleSchema(settings)
     }
 
     override fun createComplicationSlotsManager(currentUserStyleRepository: CurrentUserStyleRepository): ComplicationSlotsManager {
@@ -97,21 +107,24 @@ class ThisIsWatchFace : WatchFaceService() {
 class ThisIsSharedAssets : Renderer.SharedAssets {
     lateinit var backgroundPaint: Paint
     lateinit var backgroundBitmap: Bitmap
-    lateinit var grayBackgroundBitmap: Bitmap
+    var ambientBackgroundBitmap: Bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
     lateinit var backgroundRect: Rect
     lateinit var timeRect: Rect
     var hoursRight: Float = 0f
     var minutesLeft: Float = 0f
     lateinit var timePaint: Paint
+    lateinit var timeAmbientPaint: Paint
     lateinit var titlePaint: Paint
+    lateinit var titleAmbientPaint: Paint
     lateinit var complicationPaint: Paint
     lateinit var weekDaysNames: Array<String>
     var lineSpace = 0f
     var ratio = 1f
+    lateinit var ambientModeType: AmbientModeType
 
     override fun onDestroy() {
         backgroundBitmap.recycle()
-        grayBackgroundBitmap.recycle()
+        ambientBackgroundBitmap.recycle()
     }
 
     fun updateRatio(context: Context, ratio: Float) {
@@ -130,14 +143,66 @@ class ThisIsSharedAssets : Renderer.SharedAssets {
 
         this.ratio = ratio
     }
+
+    fun updateAmbientModeType(context: Context, trueAmbient: AmbientModeType) {
+        this.ambientModeType = trueAmbient
+        createAmbientBackground(context)
+        createAmbientPaint(context)
+    }
+
+    private fun createAmbientBackground(context: Context) {
+        ambientBackgroundBitmap.recycle()
+        if (ambientModeType == AmbientModeType.Minimalistic) {
+            ambientBackgroundBitmap = BitmapFactory.decodeResource(context.resources, R.drawable.ambient_background)
+        } else {
+            ambientBackgroundBitmap = Bitmap.createBitmap(backgroundBitmap.width, backgroundBitmap.height, Bitmap.Config.ARGB_8888)
+            val c = Canvas(ambientBackgroundBitmap)
+            val paint = Paint()
+            val cm = ColorMatrix().apply {
+                setSaturation(0f)
+            }
+            val rect = Rect(0, 0, backgroundBitmap.width, backgroundBitmap.height)
+            val f = ColorMatrixColorFilter(cm)
+            paint.colorFilter = f;
+            c.drawBitmap(backgroundBitmap, rect, rect, paint)
+        }
+    }
+
+    private fun createAmbientPaint(context: Context) {
+        if (ambientModeType == AmbientModeType.Minimalistic) {
+            titleAmbientPaint = Paint().apply {
+                color = context.resources.getColor(R.color.title_ambient_color)
+                strokeWidth = context.resources.getDimension(R.dimen.title_stroke)
+                typeface = titlePaint.typeface
+                textSize = context.resources.getDimension(R.dimen.title_size)
+                isAntiAlias = true
+                strokeCap = Paint.Cap.ROUND
+            }
+            timeAmbientPaint = Paint().apply {
+                color = context.resources.getColor(R.color.time_ambient_color)
+                strokeWidth = context.resources.getDimension(R.dimen.time_stroke)
+                typeface = timePaint.typeface
+                textSize = context.resources.getDimension(R.dimen.time_size)
+                isAntiAlias = true
+                strokeCap = Paint.Cap.ROUND
+            }
+        } else {
+            titleAmbientPaint = titlePaint
+            timeAmbientPaint = timePaint
+        }
+    }
 }
+
+private class WatchfaceConfig(
+    var ambientModeType: AmbientModeType = AmbientModeType.Minimalistic
+)
 
 class ThisIsCanvasRenderer(
     private val context: Context,
     surfaceHolder: SurfaceHolder,
     private val watchState: WatchState,
     private val complicationSlotsManager: ComplicationSlotsManager,
-    currentUserStyleRepository: CurrentUserStyleRepository,
+    private val currentUserStyleRepository: CurrentUserStyleRepository,
     canvasType: Int
 ) : Renderer.CanvasRenderer2<ThisIsSharedAssets>(
     surfaceHolder,
@@ -148,26 +213,31 @@ class ThisIsCanvasRenderer(
     false
 ) {
 
+    private val config = WatchfaceConfig()
+
     private val scope: CoroutineScope =
         CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+
+    init {
+        scope.launch {
+            currentUserStyleRepository.userStyle.collect { userStyle ->
+                for (option in userStyle) {
+                    when(option.key.id.toString()) {
+                        STYLE_KEY_AMBIENT -> {
+                            val op = option.value as? UserStyleSetting.BooleanUserStyleSetting.BooleanOption
+                            config.ambientModeType = if (op?.value == true) AmbientModeType.Background else AmbientModeType.Minimalistic
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     override suspend fun createSharedAssets(): ThisIsSharedAssets = ThisIsSharedAssets().apply {
         backgroundPaint = Paint().apply {
             color = Color.BLACK
         }
         backgroundBitmap = BitmapFactory.decodeResource(context.resources, R.drawable.watch_background)
-
-        grayBackgroundBitmap = Bitmap.createBitmap(backgroundBitmap.width, backgroundBitmap.height, Bitmap.Config.ARGB_8888)
-        val c = Canvas(grayBackgroundBitmap)
-        val paint = Paint()
-        val cm = ColorMatrix().apply {
-            setSaturation(0f)
-        }
-        val rect = Rect(0, 0, backgroundBitmap.width, backgroundBitmap.height)
-        val f = ColorMatrixColorFilter(cm)
-        paint.colorFilter = f;
-        c.drawBitmap(backgroundBitmap, rect, rect, paint)
-
         backgroundRect = Rect(0, context.resources.getDimensionPixelSize(R.dimen.background_offset_top), backgroundBitmap.width, backgroundBitmap.width)
 
         val font = Typeface.createFromAsset(context.assets, TYPEFACE_FILE_NAME)
@@ -197,6 +267,8 @@ class ThisIsCanvasRenderer(
 
         weekDaysNames = context.resources.getStringArray(R.array.week_day)
         lineSpace = context.resources.getDimension(R.dimen.title_padding)
+
+        updateAmbientModeType(context, config.ambientModeType)
     }
 
     override fun render(canvas: Canvas, bounds: Rect, zonedDateTime: ZonedDateTime, sharedAssets: ThisIsSharedAssets) {
@@ -205,12 +277,27 @@ class ThisIsCanvasRenderer(
             sharedAssets.updateRatio(context, ratio)
         }
 
+        if (config.ambientModeType != sharedAssets.ambientModeType) {
+            sharedAssets.updateAmbientModeType(context, config.ambientModeType)
+        }
+
         val isAmbient = watchState.isAmbient.value == true
         val bitmap = if (isAmbient) {
-            sharedAssets.grayBackgroundBitmap
+            sharedAssets.ambientBackgroundBitmap
         } else {
             sharedAssets.backgroundBitmap
         }
+        val timePaint = if (isAmbient) {
+            sharedAssets.timeAmbientPaint
+        } else {
+            sharedAssets.timePaint
+        }
+        val titlePaint = if (isAmbient) {
+            sharedAssets.titleAmbientPaint
+        } else {
+            sharedAssets.titlePaint
+        }
+
         canvas.drawBitmap(bitmap, sharedAssets.backgroundRect, bounds, sharedAssets.backgroundPaint)
 
         for ((_, complication) in complicationSlotsManager.complicationSlots) {
@@ -223,20 +310,20 @@ class ThisIsCanvasRenderer(
 
         // Draw time
         var baseLine = sharedAssets.timeRect.bottom.toFloat()
-        canvas.drawText(":", sharedAssets.hoursRight, baseLine, sharedAssets.timePaint)
+        canvas.drawText(":", sharedAssets.hoursRight, baseLine, timePaint)
         val hours = zonedDateTime.hour.toString()
-        var left = sharedAssets.hoursRight - sharedAssets.timePaint.measureText(hours)
-        canvas.drawText(hours, left, baseLine, sharedAssets.timePaint)
+        var left = sharedAssets.hoursRight - timePaint.measureText(hours)
+        canvas.drawText(hours, left, baseLine, timePaint)
         val minutes = zonedDateTime.minute.toString().padStart(2, '0')
-        canvas.drawText(minutes, sharedAssets.minutesLeft, baseLine, sharedAssets.timePaint)
+        canvas.drawText(minutes, sharedAssets.minutesLeft, baseLine, timePaint)
 
         // Draw date and title
-        baseLine += sharedAssets.timePaint.fontMetricsInt.top
+        baseLine += timePaint.fontMetricsInt.top
         baseLine -= sharedAssets.lineSpace
         val weekDay = sharedAssets.weekDaysNames[zonedDateTime.dayOfWeek.ordinal]
         val title = context.resources.getString(R.string.title, weekDay, zonedDateTime.dayOfMonth, zonedDateTime.month.value)
-        left = sharedAssets.timeRect.centerX() - sharedAssets.titlePaint.measureText(title) / 2
-        canvas.drawText(title, left, baseLine, sharedAssets.titlePaint)
+        left = sharedAssets.timeRect.centerX() - titlePaint.measureText(title) / 2
+        canvas.drawText(title, left, baseLine, titlePaint)
     }
 
     override fun renderHighlightLayer(canvas: Canvas, bounds: Rect, zonedDateTime: ZonedDateTime, sharedAssets: ThisIsSharedAssets) {
